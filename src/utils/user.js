d264f5faed44221fcff0ebb98faf16b1559a0c4a
rajshekhar jagadi <a.jagadi@outlook.com>
2020-07-30 13:55:37 +0530

    checking in missing file in the prev checking

--- src/utils/user.js
+++ src/utils/user.js
@@ -1,39 +1,38 @@
 const users = []
 //add user, remove user, get user, get user in room
 
-const addUser = ({id, username, room})=>{
+const addUser = ({ id, username, room }) => {
     //clean the data
     username = username.trim().toLowerCase()
     room = room.trim().toLowerCase()
 
     //validate data
-    if(!username || !room){
+    if (!username || !room) {
         return {
             error: 'Username and room are required'
         }
     }
 
     //check for existing user
-    const existingUser = users.find((user)=>{
+    const existingUser = users.find((user) => {
         return user.room === room && user.username === username
     })
 
     //validate username
-    if(existingUser)
-    {
-        return {error: 'User is in use'}
+    if (existingUser) {
+        return { error: 'User is in use' }
     }
 
     //store user
-    const user = {id, username, room}
+    const user = { id, username, room }
     users.push(user)
-    return {user}
+    return { user }
 
 }
 
-const removeUser = (id)=>{
-    const index = users.findIndex((user)=> user.id === id)
-    if(index === -1){
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id)
+    if (index === -1) {
         return undefined
     }
 
@@ -42,21 +41,20 @@ const removeUser = (id)=>{
 
 
 
-const getUser = (id)=>{
-    const user = users.find((user)=> user.id === id)
-    if(!user)
-    {
+const getUser = (id) => {
+    const user = users.find((user) => user.id === id)
+    if (!user) {
         return undefined
     }
     return user
 }
 
-const getUsersInRoom = (room)=>{
+const getUsersInRoom = (room) => {
     room = room.trim().toLowerCase()
-    return users.filter((user)=> user.room === room)
+    return users.filter((user) => user.room === room)
 }
 
-module.exports={
+module.exports = {
     addUser,
     removeUser,
     getUser,
